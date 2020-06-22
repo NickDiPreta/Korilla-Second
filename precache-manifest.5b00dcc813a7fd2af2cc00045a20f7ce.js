a6ee1ee3c3b5ba95465f5f6e9c6d0bd2715ae2d3
nick <nicholasdipreta2020@gmail.com>
2020-06-21 22:42:21 -0400

    Updates

---
+++ precache-manifest.5b00dcc813a7fd2af2cc00045a20f7ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efb75f4ac3e117efb65eb35f0d902ea",
+    "url": "/Korilla-Second/index.html"
+  },
+  {
+    "revision": "21784c6dfe1562cd5cf0",
+    "url": "/Korilla-Second/static/css/main.a33af1a3.chunk.css"
+  },
+  {
+    "revision": "3a58accdbf65b06a38d4",
+    "url": "/Korilla-Second/static/js/2.76fd22fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Korilla-Second/static/js/2.76fd22fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21784c6dfe1562cd5cf0",
+    "url": "/Korilla-Second/static/js/main.0e4e47cb.chunk.js"
+  },
+  {
+    "revision": "0c408dd62bc90edc772b",
+    "url": "/Korilla-Second/static/js/runtime-main.e8f943b6.js"
+  }
+]);
